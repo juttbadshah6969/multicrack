6af5e02495ea09df5f2408a5795206fd82c48bf1
JuttBadshah <85131357+juttbadshah6969@users.noreply.github.com>
2021-07-08 01:39:38 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#Facebook : Jutt Badshah
+#Github   : https://github.com/juttbadshah6969
+#Whatsapp : +923100209977
+clear
+echo
+echo "Install All You Need, Patient Bro ..!"
+echo
+pkg update && upgrade
+pkg install python
+pip install bs4
+pip install requests
+pip install mechanize
+pip install futures
+apt update
+apt upgrade
+pkg install python
+pkg install python2
+pip2 install requests
+pip2 install mechanize
+pip2 install bs4
+pip2 install lolcat
+pkg install nodejs-lts
+pip2 install requests
+pkg install nodejs
